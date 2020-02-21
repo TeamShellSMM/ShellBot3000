a27e9d3456e9d24be5e22680839e299515314e5e
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-02-21 04:23:51 +0000

    Replace google_spreadsheet module with ported gs_query. Filtered pending initates to only members in server

---
+++ GS.js
@@ -0,0 +1,150 @@
+'use strict'
+const request = require('request-promise')
+const { google } = require("googleapis");
+const querystring = require("querystring");
+
+
+var GS=function(config){
+  let jwtClient = new google.auth.JWT(
+         config.client_email,
+         null,
+         config.private_key,
+         ["https://spreadsheets.google.com/feeds"]
+  );
+
+  let google_auth=null
+  async function get_token(){
+    if (!(google_auth && google_auth.expiry_date > +new Date())){
+      google_auth=await jwtClient.authorize()
+    }
+    return google_auth
+  }
+
+  let json_header={}
+  let SheetCache={} //not sure if I need a mutex here or not
+  this.loadSheets=async function(ranges){ //input is sheets to be loaded. load to cache to be stored
+    let authClient=await get_token()
+    ranges=ranges?ranges.join("&ranges="):""
+    let url = "https://sheets.googleapis.com/v4/spreadsheets/"+config.spreadsheetId+"/values:batchGet?ranges="+ranges
+    const response=await request( {
+      url: url,
+      method: 'GET',
+      headers:{
+        Authorization: 'Bearer ' + authClient.access_token
+      },
+      gzip: true
+    })
+    var returnData={}
+    let data=JSON.parse(response)
+
+    if(data.valueRanges){
+      for(var i=0;i<data.valueRanges.length;i++){
+        var range=data.valueRanges[i].range.split("!")
+        range=range[0].replace(/'/g,'')
+        var header=data.valueRanges[i].values.shift()
+        json_header[range]=header;
+        returnData[range]=[]
+        for(var j=0;j<data.valueRanges[i].values.length;j++){
+          var row={}
+          for(var k=0;k<header.length;k++){
+            row[header[k]]=data.valueRanges[i].values[j][k]
+          }
+          row["GS_sheet_name"]=range
+          row["GS_row_id"]=(j+2); //for r1c1. header is r1
+          returnData[range].push(row)
+        }
+        SheetCache[range]=returnData[range]
+      }
+    }
+    return returnData
+  }
+
+  //need to load 
+  this.select=function(sheet,filter){
+    return this.query(sheet,{
+      filter:filter
+    })
+  }
+  
+  this.query=function (sheet,parameters){ //may break if column named updated or row
+    var querySheet = SheetCache[sheet]
+    if(!querySheet) return "No sheet found"
+    var headers=json_header[sheet];
+    
+    var header_to_id={}
+    for(var i=0;i<headers.length;i++){
+      header_to_id[headers[i]]=i;
+    } 
+    
+    if(parameters && parameters.filter){
+      var filter=function(row){
+        var matched=true;
+        for(var f in parameters.filter){
+          if(row[f]!=parameters.filter[f]) matched=false;
+        }
+        return matched;
+      }
+    } else {
+        var filter=function(){return true;}
+    }
+
+    var ret=[]
+    for (var row = 0; row < querySheet.length; row++){
+      if(filter(querySheet[row])){
+        var obj=querySheet[row]
+        var updated={}
+        if(parameters && parameters.update){
+          var data=[]
+          for(var u in parameters.update){
+            if(row[u]!=parameters.update[u]){
+              data.push({ 
+                range: r1c1(sheet,querySheet[row]["GS_row_id"],header_to_id[u]),
+                values: [[parameters.update[u]]]
+              })
+              updated[u]=true;
+            } else {
+              updated[u]=false; 
+            }
+          }
+          
+          if(data){
+            obj.update_ranges=data
+          }
+        }
+        obj.updated=updated
+        ret.push(obj)  
+      }
+    } 
+    return (ret.length>1) ? ret : ret[0]
+  }
+
+
+  function r1c1(sheet,r,c){ //A1 = R1C1
+    return "'"+sheet+"'!r"+(r+1)+"c"+(c+1)
+  }
+
+  //store batchUpdates in a cache then run batchUpdate to save changes?
+  this.batchUpdate=async function(ranges){ //for ease of use format will be strictly r1c1
+    //'Team Shell Data Guide'!r1c2
+    let url="https://sheets.googleapis.com/v4/spreadsheets/"+config.spreadsheetId+"/values:batchUpdate"
+    var data={
+      "valueInputOption": "USER_ENTERED",
+      "data": ranges,
+      "includeValuesInResponse": false
+    }
+
+    let authClient=await get_token()
+    const response=await request( {
+      url: url,
+      method: 'POST',
+      headers:{
+        Authorization: 'Bearer ' + authClient.access_token
+      },
+      gzip: true,
+      body: JSON.stringify(data)
+    })
+    return response
+  }
+}
+
+module.exports=GS
--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-
-const GoogleSpreadsheet = require('google-spreadsheet');
 const async = require('async');
 const util = require('util');
 const config = require('./config.json');
+const GS=require('./GS.js')
+let gs=new GS(config);
  
 // spreadsheet key is the long id in the sheets URL
-const doc = new GoogleSpreadsheet(config.spreadsheetId);
-let sheet;
-const sheetLogin=util.promisify(doc.useServiceAccountAuth);
-const getInfo=util.promisify(doc.getInfo);
-const getRows=util.promisify(doc.getRows);
-
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -22,24 +14,56 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
+
 client.on('message', async msg => {
+  if(msg.author.bot) return; //maybe can reply to shellbot 2000
+  msg.content=msg.content.trim()
   if(msg.guild.id!=config.teamshell_discord_id) return false //not in teamshell discord
   if(msg.channel.id === config.botTest1){
     console.log("in bot-test")
+    if (msg.content === '!pending_initiate') {
+      await gs.loadSheets(["Raw Members"]); //when everything goes through shellbot 3000 we can do cache invalidation stuff
+      const shellcults=get_cult_members()
+      const current_shellcult=msg.guild.roles.get(config.shellcult_id).members.map(m=>m.user.id)
+      let loaded_guild=await msg.guild.fetchMembers()
+      let missing_shellcults=shellcults.filter(v=> -1 === current_shellcult.indexOf(v))
+      missing_shellcults=missing_shellcults.filter(v=> loaded_guild.members.has(v))
+      let at_str=""
+      for(let i=0;i<missing_shellcults.length;i++){
+        at_str+="<@"+missing_shellcults[i]+">\n"
+      }
+      client.channels.get(config.botTest1).send("Currently have not recieved shellcult role in discord: "+(at_str?at_str:"Noone"))
+    }
+    const command=process_msg(msg.content)
+    console.log(msg)
+    if(command){
+
+    } 
   } else if(msg.channel.id === config.shellderShellbot) {
     if (msg.content === '!pending_initiate') {
-      const shellcults=await get_cult_members()
+      await gs.loadSheets(["Raw Members"]); //when everything goes through shellbot 3000 we can do cache invalidation stuff
+      const shellcults=get_cult_members()
       const current_shellcult=msg.guild.roles.get(config.shellcult_id).members.map(m=>m.user.id)
-      const missing_shellcults=shellcults.filter(v=> -1 === current_shellcult.indexOf(v))
+      
+      let loaded_guild=await msg.guild.fetchMembers()
+      let missing_shellcults=shellcults.filter(v=> -1 === current_shellcult.indexOf(v))
+      missing_shellcults=missing_shellcults.filter(v=> loaded_guild.members.has(v))
+
       let at_str=""
       for(let i=0;i<missing_shellcults.length;i++){
         at_str+="<@"+missing_shellcults[i]+">\n"
       }
       client.channels.get(config.shellderShellbot).send("Currently have not recieved shellcult role in discord: "+(at_str?at_str:"Noone"))
     } else if (msg.content === '!tsinitiate') {
-      const shellcults=await get_cult_members()
+      await gs.loadSheets(["Raw Members"]); //when everything goes through shellbot 3000 we can do cache invalidation stuff
+      const shellcults=get_cult_members()
       const current_shellcult=msg.guild.roles.get(config.shellcult_id).members.map(m=>m.user.id)
-      const missing_shellcults=shellcults.filter(v=> -1 === current_shellcult.indexOf(v))
+      
+      let loaded_guild=await msg.guild.fetchMembers()
+      let missing_shellcults=shellcults.filter(v=> -1 === current_shellcult.indexOf(v))
+      missing_shellcults=missing_shellcults.filter(v=> loaded_guild.members.has(v))
+
       if(missing_shellcults.length>0){
         let at_str=""
         for(let i=0;i<missing_shellcults.length;i++){
@@ -55,36 +79,37 @@ client.on('message', async msg => {
 });
 
 
+//teamshell sheets
+const static_vars=[
+"TeamShell Variable","Points","TeamShell Ranks","Seasons", //static vars
+'Raw Members','Raw Levels','Raw Played' //play info
+]; //initial vars to be loaded on bot load
+const play_infos =['Raw Members','Raw Levels','Raw Played']; //to be loaded on every command? will
+
 (async () => { //main thread
-  
-
-
-
+  const response=await gs.loadSheets(static_vars) //loading initial sheets
   client.login(config.token); 
 })();
 
-
-
-async function get_sheet_by_name(name){
-  if(!doc.isAuthActive()) await sheetLogin(config);
-  const info= await getInfo()
-  return info.worksheets.find(sheet => sheet.title === name);
+function process_msg(str){
+  str=str.trim()
+  if(str.charAt(0)!="!") return false
+  str=str.split(" ")
+  const command=str.shift()
+  return {command:command.substring(1),parameters:str}
 }
 
-async function get_rows(sheetName,options){
-  const sheet=await get_sheet_by_name("Raw Members")
-  const sheetRows=util.promisify(sheet.getRows)
-  const rows=await sheetRows()
-  return rows
+function process_points(){
+
 }
 
 
-async function get_cult_members(){ //return array of discord_ids
-  const rows=await get_rows("Raw Members")
+function get_cult_members(){ //return array of discord_ids
+  const rows=gs.select("Raw Members")
   let cultmembers=[]
   for(var i=0;i<rows.length;i++){
-    if(rows[i] && rows[i].cultmember=="1" && rows[i].discordid) 
-      cultmembers.push(rows[i].discordid);  
+    if(rows[i] && rows[i].cult_member=="1" && rows[i].discord_id) 
+      cultmembers.push(rows[i].discord_id);  
   }
   return cultmembers
 }
