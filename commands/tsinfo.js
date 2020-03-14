991bce23c6a520bced2e2b9d03419b572d3123d9
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-03-14 11:40:14 +0000

    Added tsinfo

---
+++ commands/tsinfo.js
@@ -0,0 +1,40 @@
+const { Command } = require('discord-akairo');
+
+
+class tsinfo extends Command {
+    constructor() {
+        super('tsinfo', {
+           aliases: ['tsinfo','info'],
+            args: [{
+                    id: 'code',
+                    type: 'string',
+                    default: ''
+                }],
+           channelRestriction: 'guild'
+        });
+    }
+    
+    async exec(message,args) {     
+         //if(!( 
+        //    message.channel.id === ts.channels.shellderShellbot  //only in bot-test channel
+        //)) return false;
+      try {
+
+        args.code=args.code.toUpperCase();
+        if(!ts.valid_format(args.code))
+          ts.userError("You did not provide a valid format for the level");
+
+        await gs.loadSheets(["Raw Members","Raw Levels","Raw Played"]); //when everything goes through shellbot 3000 we can do cache invalidation stuff
+        const player=ts.get_user(message);
+        var level=ts.getExistingLevel(args.code)
+
+        var randomEmbed=ts.levelEmbed(level)
+
+        await message.channel.send(player.user_reply)
+        await message.channel.send(randomEmbed)
+      } catch (error) {
+        message.reply(ts.getUserErrorMsg(error))
+      }
+    }
+}
+module.exports = tsinfo;
