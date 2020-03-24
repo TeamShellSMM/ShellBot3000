af55f373d321c314ff2ed3de87f5a7126a0acbb8
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-03-24 10:43:48 +0000

    abstract out clear function

--- TS.js
+++ TS.js
@@ -8,6 +8,8 @@ const Plays = require('./models/Plays');
 
 var TS=function(gs,client){ //loaded after gs
 var ts=this
+
+
 this.valid_format=function(code){
   return /^[0-9A-Z]{3}-[0-9A-Z]{3}-[0-9A-Z]{3}$/.test(code.toUpperCase())
 }
@@ -38,7 +40,6 @@ this.checkBearerToken=async function(discord_id,token){
   }
   return true
 }
-
 
 this.login=async function(discord_id,row_id){
   let bearer=crypto.randomBytes(16).toString('hex').toUpperCase()
@@ -133,6 +134,76 @@ this.embedAddLongField=function(embed,header,body){
     embed.addField(header,bodyStr[k]);
     header = "\u200b"
   }
+}
+
+this.clear=async function(args,strOnly){
+    args.code=args.code.toUpperCase();
+    if(!ts.valid_code(args.code))
+      ts.userError("You did not provide a valid code for the level");
+
+    if(args.like=="1" || args.like.toLowerCase()=="like"){
+      args.like=1
+    }
+
+    if(args.difficulty.toLowerCase()=="like"){
+      args.difficulty=''
+      args.like=1
+    }
+
+    if(args.difficulty && !ts.valid_difficulty(args.difficulty)){
+      ts.userError("You did not provide a valid difficulty vote");
+    }
+
+    await gs.loadSheets(["Raw Members","Raw Levels"]);
+    const player=await ts.get_user(args.discord_id);
+    var level=ts.getExistingLevel(args.code)
+    var existing_play = await Plays.query()
+      .where('code','=',args.code)
+      .where('player','=',player.Name)
+      .first();
+
+    if(existing_play && existing_play.completed=="1")
+      ts.userError("You have already submitted a clear for \""+level["Level Name"]+" by "+level.Creator)
+
+    if(level.Creator==player.Name)
+      ts.userError("You can't submit a clear for your own level")
+
+    var creator=gs.select("Raw Members",{"Name":level.Creator});
+    if(creator && creator.atme=="1" && creator.discord_id && !strOnly){
+     var creator_str="<@"+creator.discord_id+">"
+    } else {
+     var creator_str=level.Creator
+    }
+    var row={
+      "code":args.code,
+      "player":player.Name,
+      "completed":"1",
+      "is_shellder":player.shelder,
+      "liked":args.like,
+      "difficulty_vote":args.difficulty
+    }
+    if(existing_play){
+      await Plays.query()
+        .findById(existing_play.id)
+        .patch(row);
+    } else {
+      await Plays.query().insert(row);
+    }
+
+    var msg=["You have cleared \""+level["Level Name"]+"\"  by "+creator_str+" "+ts.emotes.GG]
+    if(args.difficulty)
+      msg.push(" ‣You voted "+args.difficulty+" as the difficulty.");
+
+    if(level.Approved=="1"){
+      msg.push(" ‣You have earned "+ts.pointMap[parseFloat(level.Difficulty)]+" points.");
+    } else if(level.Approved=="0"){
+      msg.push(" ‣This level is still pending.")
+    }
+
+    if(args.like){
+     msg.push(" ‣You have also liked the level "+ts.emotes.love)
+    }
+    return (strOnly?"":player.user_reply)+msg.join("\n");
 }
 
 this.getExistingLevel=function(code){
--- commands/clear.js
+++ commands/clear.js
@@ -24,82 +24,9 @@ class TSClear extends Command {
 
     async exec(message,args) {
         try{
-          args.code=args.code.toUpperCase();
-          var command=ts.parse_command(message);
-          var strOnly=false;
-
-          if(!ts.valid_code(args.code))
-            ts.userError("You did not provide a valid code for the level");
-
-          if(args.like=="1" || args.like.toLowerCase()=="like"){
-            args.like=1
-          }
-
-          if(args.difficulty.toLowerCase()=="like"){
-            args.difficulty=''
-            args.like=1
-          }
-
-          if(args.difficulty && !ts.valid_difficulty(args.difficulty)){
-            ts.userError("You did not provide a valid difficulty vote");
-          }
-
-          await gs.loadSheets(["Raw Members","Raw Levels"]);
-          const player=await ts.get_user(message);
-
-          //convert below to be function
-          //call it in approve
-          var level=ts.getExistingLevel(args.code)
-
-          var existing_play = await Plays.query()
-            .where('code','=',args.code)
-            .where('player','=',player.Name)
-            .first();
-
-          if(existing_play && existing_play.completed=="1")
-            ts.userError("You have already submitted a clear for \""+level["Level Name"]+" by "+level.Creator)
-
-          if(level.Creator==player.Name)
-            ts.userError("You can't submit a clear for your own level")
-
-          var creator=gs.select("Raw Members",{"Name":level.Creator});
-          if(creator && creator.atme=="1" && creator.discord_id && !strOnly){
-           var creator_str="<@"+creator.discord_id+">"
-          } else {
-           var creator_str=level.Creator
-          }
-          var row={
-            "code":args.code,
-            "player":player.Name,
-            "completed":"1",
-            "is_shellder":player.shelder,
-            "liked":args.like,
-            "difficulty_vote":args.difficulty
-          }
-          if(existing_play){
-            console.log(existing_play);
-            await Plays.query()
-              .findById(existing_play.id)
-              .patch(row);
-          } else {
-            await Plays.query().insert(row);
-          }
-
-          var msg=["You have cleared \""+level["Level Name"]+"\"  by "+creator_str+" "+ts.emotes.GG]
-          if(args.difficulty)
-            msg.push(" ‣You voted "+args.difficulty+" as the difficulty.");
-
-          if(level.Approved=="1"){
-            msg.push(" ‣You have earned "+ts.pointMap[parseFloat(level.Difficulty)]+" points.");
-          } else if(level.Approved=="0"){
-            msg.push(" ‣This level is still pending.")
-          }
-
-          if(args.like){
-           msg.push(" ‣You have also liked the level "+ts.emotes.love)
-          }
-
-            message.channel.send(player.user_reply+msg.join("\n"))
+          args.discord_id=message.author.id
+          let msg=await ts.clear(args)
+          message.channel.send(msg)
         } catch(error){
             message.reply(ts.getUserErrorMsg(error))
         }
