5f52561d1f003af05a3344de711d5470cd27cd68
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-03-27 12:35:41 +0000

    login:added note about in-app browsers/webview for mobile

--- commands/login.js
+++ commands/login.js
@@ -14,7 +14,7 @@ class login extends Command {
           await gs.loadSheets(["Raw Members","Raw Levels"]);
           const player=await ts.get_user(message);
           let otp=await ts.generateOtp(message.author.id)
-          message.author.send(player.user_reply+" You have requested a login token for the website. click the link below to login:\n <"+config.login_url+otp+">\n This token will only be valid for 30 minutes")
+          message.author.send(player.user_reply+" You have requested a login token for the website. click the link below to login.:\n <"+config.login_url+otp+"> "+ts.emotes.bam+"\n If you're on mobile, copy the link and paste it into your preferred browser app. If you open this link in an in-app browser, your login might not be saved properly. "+ts.emotes.buzzyS+"\nThis token will only be valid for 30 minutes")
         } catch(error){
             message.reply(ts.getUserErrorMsg(error))
         }
