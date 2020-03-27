7729b18c757fad9101f15f8d62881b824eaffa0d
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-03-27 12:13:33 +0000

    Allow \!login to be called in channels. some refactors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const GS=require('./GS.js')
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
-var compression = require('compression')
+const compression = require('compression')
 
 global.db={}
 db.Tokens=require('./models/Tokens.js');
@@ -39,7 +39,7 @@ global.ts=new TS(gs,client);
 })();
 
 async function generateSiteJson(isShellder){
-  var SheetCache = gs.getArrayFormat([
+  const SheetCache = gs.getArrayFormat([
         "Raw Levels!J",
         "Seasons!B",
         "tags",
@@ -48,25 +48,25 @@ async function generateSiteJson(isShellder){
         "Points!B"
       ])
 
-    var _rawLevels = SheetCache["Raw Levels"]
-    var _seasons = SheetCache["Seasons"]
-    var tags = SheetCache["tags"];
-    var competiton_winners = SheetCache["Competition Winners"];
-    var _members = SheetCache["Raw Members"]
-    var _points = SheetCache["Points"]
+    let _rawLevels = SheetCache["Raw Levels"]
+    let _seasons = SheetCache["Seasons"]
+    let tags = SheetCache["tags"];
+    let competiton_winners = SheetCache["Competition Winners"];
+    let _members = SheetCache["Raw Members"]
+    let _points = SheetCache["Points"]
 
-    var _playedLevels = await db.Plays.query();
+    let _playedLevels = await db.Plays.query();
 
-    for(var i=0;i<_playedLevels.length;i++){
+    for(let i=0;i<_playedLevels.length;i++){
       _playedLevels[i].created_at=typeof _playedLevels[i].created_at ==="string" ? 
       (new Date(_playedLevels[i].created_at.replace(/-/g,"/"))).getTime()/1000:
       _playedLevels[i].created_at;
     }
 
-    var rawLevels=[_rawLevels[0]];
-    var reuploaded=[]
-    var pending=[]
-    for(var i=1;i<_rawLevels.length;i++){
+    let rawLevels=[_rawLevels[0]];
+    let reuploaded=[]
+    let pending=[]
+    for(let i=1;i<_rawLevels.length;i++){
       if(_rawLevels[i][4]=="0"){
         pending.push(_rawLevels[i][0])
       }
@@ -79,27 +79,27 @@ async function generateSiteJson(isShellder){
     }
 
 
-    var tag_header=tags.shift()
-    var comp_winners_header=competiton_winners.shift()
-    var _tags={}
-    var _seperate=[]
-    for(var i=0;i<tags.length;i++){
+    let tag_header=tags.shift()
+    let comp_winners_header=competiton_winners.shift()
+    let _tags={}
+    let _seperate=[]
+    for(let i=0;i<tags.length;i++){
       if(tags[i][2]=="1"){
         _seperate.push(tags[i][0])
       }
       _tags[tags[i][0]]=tags[i][1]
     }
 
-    var seasons_header=_seasons.shift()
-    var Seasons=[]
-    for(var i=0;i<_seasons.length;i++){
+    let seasons_header=_seasons.shift()
+    let Seasons=[]
+    for(let i=0;i<_seasons.length;i++){
       Seasons.push({
         name:_seasons[i][1],
         startdate:_seasons[i][0]
       })
     }
 
-    for(var i=0;i<_members.length;i++){
+    for(let i=0;i<_members.length;i++){
       if(_members[i].length==2){
         _members[i].push("")
       } else if(_members[i].length==1){ //so hack
@@ -109,12 +109,12 @@ async function generateSiteJson(isShellder){
     }
 
 
-    var d=new Date()
-    var day = d.getDay();
-    var DAY_START=(new Date(d.getFullYear(), d.getMonth(), d.getDate())).getTime()/1000;
-    var WEEK_START=(new Date(d.getFullYear(), d.getMonth(), d.getDate() + (day == 0?-6:1)-day )).getTime()/1000;
-    var MONTH_START=(new Date(d.getFullYear(), d.getMonth(), 1)).getTime()/1000;
-    var json={
+    let d=new Date()
+    let day = d.getDay();
+    let DAY_START=(new Date(d.getFullYear(), d.getMonth(), d.getDate())).getTime()/1000;
+    let WEEK_START=(new Date(d.getFullYear(), d.getMonth(), d.getDate() + (day == 0?-6:1)-day )).getTime()/1000;
+    let MONTH_START=(new Date(d.getFullYear(), d.getMonth(), 1)).getTime()/1000;
+    let json={
       "lastUpdated":gs.lastUpdated,
       "DAY_START":DAY_START,
       "WEEK_START":WEEK_START,
@@ -131,16 +131,16 @@ async function generateSiteJson(isShellder){
     };
     if(isShellder){
       //console.log(pending)
-      var _comments=db.PendingVotes.query().where("is_shelder",1)
+      let _comments=db.PendingVotes.query().where("is_shelder",1)
       //console.log(_comments)
-      var comments={}
-      for(var i=0;i<_comments.length;i++){
-        var currCode=_comments[i].code
+      let comments={}
+      for(let i=0;i<_comments.length;i++){
+        let currCode=_comments[i].code
         if(pending.indexOf(currCode)!="-1"){
           if(!comments[currCode]){
             comments[currCode]=[]
           }
-          var temp=Object.assign({},_comments[i])
+          let temp=Object.assign({},_comments[i])
           delete temp.Code
           comments[currCode].push(temp)
         }
@@ -155,8 +155,8 @@ async function generateSiteJson(isShellder){
 
 app.get('/json', async (req, res) => {
 
-  var lastUpdated = gs.lastUpdated
-
+  let lastUpdated = gs.lastUpdated
+  let json = null;
   if(req.query.lastLoaded==lastUpdated){
     json = "No Updated Needed"
   } else {
@@ -172,7 +172,7 @@ app.post('/json',async (req,res)=>{
 
     if(req.body.token && req.body.discord_id){
       await ts.checkBearerToken(req.body.discord_id,req.body.token)
-      var user=await ts.get_user(req.body.discord_id)
+      let user=await ts.get_user(req.body.discord_id)
       //console.log(user)
     }
 
@@ -187,11 +187,11 @@ app.post('/json',async (req,res)=>{
 app.post('/clear',async (req,res)=>{
     try {
       if(req.body.token){
-        var discord_id=await ts.checkBearerToken(req.body.discord_id,req.body.token)
-        var user=await ts.get_user(discord_id)
+        let discord_id=await ts.checkBearerToken(req.body.discord_id,req.body.token)
+        let user=await ts.get_user(discord_id)
       }
       req.body.discord_id=discord_id
-      var msg=await ts.clear(req.body)
+      let msg=await ts.clear(req.body)
 
       await client.channels.get(ts.channels.clearSubmit).send(msg)
       json = {status:"sucessful",msg:msg}
@@ -206,8 +206,8 @@ app.post('/clear',async (req,res)=>{
 app.post('/approve',async (req,res)=>{
     try {
       if(req.body.token){
-        var discord_id=await ts.checkBearerToken(req.body.discord_id,req.body.token)
-        var user=await ts.get_user(discord_id)
+        let discord_id=await ts.checkBearerToken(req.body.discord_id,req.body.token)
+        let user=await ts.get_user(discord_id)
       }
 
       if(user.shelder!="1"){
@@ -218,8 +218,8 @@ app.post('/approve',async (req,res)=>{
 
       req.body.reason=req.body.comment
       
-      var msg=await ts.approve(req.body)
-      var clearmsg=await ts.clear(req.body)
+      let msg=await ts.approve(req.body)
+      let clearmsg=await ts.clear(req.body)
 
       await client.channels.get(ts.channels.clearSubmit).send(clearmsg)
 
@@ -237,21 +237,21 @@ app.post('/approve',async (req,res)=>{
 app.post('/json/login', async (req, res) => {
   try{
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    var returnObj={}
+    let returnObj={}
 
     if(!req.body.otp) ts.userError("No OTP provided")
 
-    var token=await db.Tokens.query()
+    let token=await db.Tokens.query()
       .where('token','=',req.body.otp)
 
     if(token.length){
       token=token[0]
-      var tokenExpireAt=moment(token.created_at).add(30,'m').valueOf()
-      var now=moment().valueOf()
+      let tokenExpireAt=moment(token.created_at).add(30,'m').valueOf()
+      let now=moment().valueOf()
       if(tokenExpireAt<now)
         ts.userError("The OTP password given is already expired")
-      var user=await ts.get_user(token.discord_id);
-      var bearer=await ts.login(token.discord_id,token.id)
+      let user=await ts.get_user(token.discord_id);
+      let bearer=await ts.login(token.discord_id,token.id)
       returnObj={status:"logged_in",type:"bearer","discord_id":user.discord_id,"token":bearer,"user_info":user}
     } else {
       ts.userError("Your one time password was incorrect. You can DM ShellBot with !login to get another code")
--- commands/login.js
+++ commands/login.js
@@ -4,19 +4,17 @@ class login extends Command {
     constructor() {
         super('login', {
            aliases: ['login'],
-           channelRestriction: 'dm'
+           cooldown: 5000
         });
     }
 
     async exec(message,args) {
         try{
-          var command=ts.parse_command(message);
-
+          let command=ts.parse_command(message);
           await gs.loadSheets(["Raw Members","Raw Levels"]);
           const player=await ts.get_user(message);
-          var otp=await ts.generateOtp(message.author.id)
-          //message.channel.send(player.user_reply+" You have requested a login token for the website. copy the code below ```"+otp+"``` This token will only be valid for 30 minutes")
-          message.channel.send(player.user_reply+" You have requested a login token for the website. click the link below to login:\n <"+config.login_url+otp+">\n This token will only be valid for 30 minutes")
+          let otp=await ts.generateOtp(message.author.id)
+          message.author.send(player.user_reply+" You have requested a login token for the website. click the link below to login:\n <"+config.login_url+otp+">\n This token will only be valid for 30 minutes")
         } catch(error){
             message.reply(ts.getUserErrorMsg(error))
         }
