ded2999e2c78607c9a588bce6ee225e868332e5e
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-04-20 12:30:03 +0000

    expanded reload/refresh roles

--- commands/ReloadCommand.js
+++ commands/ReloadCommand.js
@@ -1,16 +1,29 @@
 const { Command } = require('discord-akairo');
+const config = require('../config.json');
 
 class ReloadCommand extends Command {
     constructor() {
         super('reload', {
             aliases: ['reload'],
-            ownerOnly: true,
-            category: 'owner'
         });
+    }
+
+    canRun(message){
+        if(config.ownerID && config.ownerID.indexOf(message.author.id)!==-1){
+            return true;
+        }
+        if(config.devs && config.devs.indexOf(message.author.id)!==-1){
+            return true;
+        }        
+        return false;
     }
 
     async exec(message, args) {
         // `this` refers to the command object.
+        if(!this.canRun(message)){
+            return false;
+        }
+
         await this.handler.reloadAll();
         return message.reply(`Reloaded commands`);
     }
--- commands/ReloadData.js
+++ commands/ReloadData.js
@@ -1,15 +1,36 @@
 const TSCommand = require('../TSCommand.js');
+const config = require('../config.json');
 
 class ReloadData extends TSCommand {
     constructor() {
         super('refresh', {
             aliases: ['refresh'],
-            ownerOnly: true,
-            category: 'owner'
+            channelRestriction: 'guild'
         });
     }
 
+    canRun(ts,message){
+        if(config.ownerID && config.ownerID.indexOf(message.author.id)!==-1){
+            return true;
+        }
+        if(config.devs && config.devs.indexOf(message.author.id)!==-1){
+            return true;
+        }
+        let player=ts.gs.select("Raw Members",{"discord_id":message.author.id,"shelder":"1"},true)
+        if(player.length>0){
+            return true
+        }
+        
+        return false;
+    }
+    
+
     async tsexec(ts,message, args){
+        
+        if(!this.canRun(ts,message)){
+            return false;
+        }
+
         await ts.load()
         return message.reply(`Reloaded data!`);
     }
