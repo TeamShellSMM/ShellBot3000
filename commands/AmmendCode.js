d2554f482b82c1ba331efb249b268e2a26a287ee
a_unique_id <45527149+a-unique-id@users.noreply.github.com>
2020-05-15 12:20:09 +0000

    minor refactor

--- commands/AmmendCode.js
+++ commands/AmmendCode.js
@@ -1,6 +1,7 @@
 const TSCommand = require('../TSCommand.js');
 /* istanbul ignore next */
-const config = require('../config.json')[process.env.NODE_ENV || 'development']
+var environment = process.env.NODE_ENV || 'development'
+const config = require('../config.json')[environment]
 class AmmendCode extends TSCommand {
   constructor() {
   super('ammendcode', {
--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
 /* istanbul ignore next */
-var environment = process.env.NODE_ENV || 'development'
-var config = require('../knexfile.js')[environment]
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile.js')[environment]
 module.exports = require('knex')(config)
